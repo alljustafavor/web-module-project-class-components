1efd05f273b1a0918722ae96dc0551310240cf4f
Joseph Noblett <noblettjoe@gmail.com>
2023-09-17 01:25:34 -0500

    init commit, built comps, added handlers

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import Form from './Form'
+import TodoList from './TodoList'
+
+let id = 0;
+const getId = () => ++id;
+
+const initialTodo = [
+  {id: getId(), name: 'Walk the dog', completed: false},
+  {id: getId(), name: 'learn React', completed: true},
+  {id: getId(), name: 'Use Postman', completed: false}
+]
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: initialTodo
+    }
+  }
+
+  addTodo = (name) => {
+    this.setState({
+      ...this.todos,
+      todos: this.state.todos.concat({ id: getId(), completed: false, name })
+    })
+  }
+
+  toggleCompletion = (id) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td => {
+        if (id == td.id) return { ...td, completed: !td.completed }
+        return td;
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos}  toggleCompletion={this.toggleCompletion} />
+        <Form addTodo={this.addTodo} />
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ''
+    }
+  }
+  onSubmit = evt => {
+    evt.preventDefault();
+    this.props.addTodo(this.state.name)
+  }
+
+  onChange = evt => {
+    const { value } = evt.target;
+    this.setState({
+      name: value
+    })
+  }
+
   render() {
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.name} placeholder='add a task' onChange={this.onChange}/>
+          <input type='submit' />
+        <div>
+          <button>Completed</button>
+        </div>
+      </form>
     )
   }
 }
